86e4b569c3ee4e78b273d9ade8589acd7ded0f50
Florian-Haas <fhaasbusiness@gmail.com>
2025-02-13 20:21:53 +0100

    Create enableChainSepolia.ts and enableChainZKsync.ts

---
+++ scripts/enableChainSepolia.ts
@@ -0,0 +1,39 @@
+// scripts/enableChain.ts
+
+import { network } from "hardhat";
+import { ethers, Wallet } from "ethers";
+import { XNFT, XNFT__factory } from "../typechain-types";
+
+async function main() {
+  if (network.name !== `sepolia`) {
+    console.error(`Must be called from Ethereum Sepolia`);
+    return 1;
+  }
+
+  const privateKey = process.env.PRIVATE_KEY!;
+  const rpcProviderUrl = process.env.SEPOLIA_RPC_URL;
+
+  const provider = new ethers.JsonRpcProvider(rpcProviderUrl);
+  const wallet = new Wallet(privateKey);
+  const signer = wallet.connect(provider);
+
+  const xNftAddressSepolia = `YOUR_SEPOLIA_ADDRESS_HERE`;
+  const xNftAddressZKsync = `YOUR_ZKSYNC_ADDRESS_HERE`;
+  const chainSelectorZKsync = `6898391096552792247`;
+  const ccipExtraArgs = `0x97a657c90000000000000000000000000000000000000000000000000000000000030d40`;
+
+  const xNft: XNFT = XNFT__factory.connect(xNftAddressSepolia, signer);
+
+  const tx = await xNft.enableChain(
+      chainSelectorZKsync,
+      xNftAddressZKsync,
+      ccipExtraArgs
+  );
+
+  console.log(`Transaction hash: ${tx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ scripts/enableChainZKsync.ts
@@ -0,0 +1,39 @@
+// scripts/enableChain.ts
+
+import { network } from "hardhat";
+import { ethers, Wallet } from "ethers";
+import { XNFT, XNFT__factory } from "../typechain-types";
+
+async function main() {
+  if (network.name !== `zkSyncSepoliaTestnet`) {
+    console.error(`Must be called from zkSync Sepolia`);
+    return 1;
+  }
+
+  const privateKey = process.env.PRIVATE_KEY!;
+  const rpcProviderUrl = process.env.ZKSYNC_RPC_URL;
+
+  const provider = new ethers.JsonRpcProvider(rpcProviderUrl);
+  const wallet = new Wallet(privateKey);
+  const signer = wallet.connect(provider);
+
+  const xNftAddressZKsync = `YOUR_ZKSYNC_ADDRESS_HERE`;
+  const xNftAddressSepolia = `YOUR_SEPOLIA_ADDRESS_HERE`;
+  const chainSelectorSepolia = `16015286601757825753`;
+  const ccipExtraArgs = `0x97a657c90000000000000000000000000000000000000000000000000000000000030d40`;
+
+  const xNft: XNFT = XNFT__factory.connect(xNftAddressZKsync, signer);
+
+  const tx = await xNft.enableChain(
+      chainSelectorSepolia,
+      xNftAddressSepolia,
+      ccipExtraArgs
+  );
+
+  console.log(`Transaction hash: ${tx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
